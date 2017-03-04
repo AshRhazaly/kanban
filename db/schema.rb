c5e1a6875c5452af38d34239c445b4d062d0ddcc
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-03-04 18:36:32 +0800

    Add content column to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170301152713) do
+ActiveRecord::Schema.define(version: 20170304103607) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "title"
+    t.text     "content"
   end
 
 end

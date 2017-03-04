007ff4fd63d819c0658d8289e71bfd3ab6616858
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-03-04 18:35:15 +0800

    Test passed for post title

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be invalid without a title" do
+    posts(:one).title  = nil
+    assert_nil posts(:one).title
+    assert_equal false, posts(:one).valid?
+  end
 end

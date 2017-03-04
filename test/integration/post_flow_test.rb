96583283444c858c65b979698f1e2e2b8d873d89
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-03-04 20:33:57 +0800

    Add test for home page

--- test/integration/post_flow_test.rb
+++ test/integration/post_flow_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class PostFlowTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "post index is home page" do
+    get "/"
+    assert_select "h1", "Post#index"
+  end
 end

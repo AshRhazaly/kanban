6857a63a0a769a3a8f33c674de4cc2d7c3355ebf
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-03-05 16:41:56 +0800

    Add post id to task

---
+++ db/migrate/20170305084144_add_post_id_to_task.rb
@@ -0,0 +1,5 @@
+class AddPostIdToTask < ActiveRecord::Migration[5.0]
+  def change
+    add_column :tasks, :post_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170305083949) do
+ActiveRecord::Schema.define(version: 20170305084144) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170305083949) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text     "content"
+    t.integer  "post_id"
   end
 
 end

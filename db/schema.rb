fe4cd3c86168cc676881a08b67cc5a5950ec5809
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-03-05 16:40:08 +0800

    Add content column to task table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170304143257) do
+ActiveRecord::Schema.define(version: 20170305083949) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170304143257) do
   create_table "tasks", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "content"
   end
 
 end

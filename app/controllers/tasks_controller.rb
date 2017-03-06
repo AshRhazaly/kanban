6236f94b37ad437b87c568ad22fe6af5324dae5c
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-03-06 11:27:39 +0800

    Add ajax post to database on task model

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,26 @@
 class TasksController < ApplicationController
   before_action :set_tasks, only: [:create,:show,:edit,:update,:destroy]
 
+  def index
+  end
+
   def new
+    @task = @post.tasks.build
   end
 
   def create
+    @task = @post.tasks.build(tasks_params)
+
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to @post, notice: 'Task was successfully created.' }
+        format.js   {}
+        format.json { render json: @task, status: :created, location: @task }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,8 @@
 Rails.application.routes.draw do
-  get 'tasks/new'
 
-  get 'tasks/create'
-
-  get 'tasks/show'
-
-  get 'tasks/edit'
-
-  get 'tasks/update'
-
-  get 'tasks/destroy'
-
-  resources :posts
+  resources :posts do
+    resources :tasks
+  end
   root "posts#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
